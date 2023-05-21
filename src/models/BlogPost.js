7e1d71cd5114e94b5b7eb24e182e20176609ee36
Felipe Gomes <fdefariasgomes@gmail.com>
2023-05-21 10:27:34 -0300

    fix(models/BlogPost): add published & updated properties

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -9,6 +9,8 @@ const BlogPostSchema = (sequelize, DataTypes) => {
       title: { type: DataTypes.STRING },
       content: { type: DataTypes.STRING },
       userId: { type: DataTypes.INTEGER },
+      published: { type: DataTypes.DATE },
+      updated: { type: DataTypes.DATE },
     },
     {
       tableName: 'blog_posts',
