9998af7080fb932fb40293f1452c4da6f3c83cbc
Felipe Gomes <fdefariasgomes@gmail.com>
2023-05-21 08:49:17 -0300

    feat(migrations/blog-posts): add undo migration function

--- src/migrations/20230518123746-create-blog-posts.js
+++ src/migrations/20230518123746-create-blog-posts.js
@@ -40,11 +40,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('blog_posts');
   },
 };
