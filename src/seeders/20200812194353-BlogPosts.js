2715c74c1453980a7353f38075d77c08ec300236
Felipe Gomes <fdefariasgomes@gmail.com>
2023-05-27 15:11:49 -0300

    refact(sequelize/seeders): change users and blog posts values

--- src/seeders/20200812194353-BlogPosts.js
+++ src/seeders/20200812194353-BlogPosts.js
@@ -1,24 +1,24 @@
 module.exports = {
   up: async (queryInterface, _Sequelize) => {
-    await queryInterface.bulkInsert('blog_posts',
-      [
-        {
-          id: 1,
-          title: 'Post do Ano',
-          content: 'Melhor post do ano',
-          user_id: 1,
-          published: new Date('2011-08-01T19:58:00.000Z'),
-          updated: new Date('2011-08-01T19:58:51.000Z'),
-        },
-        {
-          id: 2,
-          title: 'Vamos que vamos',
-          content: 'Foguete não tem ré',
-          user_id: 1,
-          published: new Date('2011-08-01T19:58:00.000Z'),
-          updated: new Date('2011-08-01T19:58:51.000Z'),
-        },
-      ]);
+    await queryInterface.bulkInsert('blog_posts', [
+      {
+        id: 1,
+        title: 'Consectetur excepteur.',
+        content: `Veniam amet fugiat consectetur officia do quis aute amet.`,
+        user_id: 1,
+        published: new Date('2011-08-01T19:58:00.000Z'),
+        updated: new Date('2011-08-01T19:58:51.000Z'),
+      },
+      {
+        id: 2,
+        title: 'Ipsum amet amet magna id.',
+        content:
+          'Consequat in officia cupidatat veniam magna adipisicing qui dolor occaecat enim id sit ad.',
+        user_id: 1,
+        published: new Date('2011-08-01T19:58:00.000Z'),
+        updated: new Date('2011-08-01T19:58:51.000Z'),
+      },
+    ]);
   },
 
   down: async (queryInterface, _Sequelize) => {
